8d08978741f86eb35cc1375f4f75ba1011f66ecd
SzczepanPrzebieda <szczepanprzebieda@gmail.com>
2016-12-13 12:01:01 +0000

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app works!';
+  
+  
+  constructor(private router: Router) { }
+
+
+  navigate() {
+        this.router.navigate(["home"])
+        this.router.navigate(["part-a"]),
+        this.router.navigate(["part-b"]);
+    }
+
 }
